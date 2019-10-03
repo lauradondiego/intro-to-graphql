4cb7d2b51562a86b0e876abf84c7c0f4f0f2f4e9
laura <lauradondiego@gmail.com>
2019-10-03 13:55:43 -0400

    added server code

--- src/server.js
+++ src/server.js
@@ -1,33 +1,47 @@
-import { ApolloServer } from 'apollo-server'
-import { loadTypeSchema } from './utils/schema'
-import { merge } from 'lodash'
-import config from './config'
-import { connect } from './db'
-import product from './types/product/product.resolvers'
-import coupon from './types/coupon/coupon.resolvers'
-import user from './types/user/user.resolvers'
+import { ApolloServer } from 'apollo-server';
+import { loadTypeSchema } from './utils/schema';
+import { merge } from 'lodash';
+import config from './config';
+import { connect } from './db';
+import product from './types/product/product.resolvers';
+import coupon from './types/coupon/coupon.resolvers';
+import user from './types/user/user.resolvers';
 
-const types = ['product', 'coupon', 'user']
+const types = [ 'product', 'coupon', 'user' ];
 
 export const start = async () => {
-  const rootSchema = `
-    schema {
-      query: Query
-    }
-  `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+	const rootSchema = `
+   type Cat {
+     name: String
+   }
 
-  const server = new ApolloServer({
-    typeDefs: [rootSchema],
-    resolvers: {},
-    context({ req }) {
-      // use the authenticate function from utils to auth req, its Async!
-      return { user: null }
-    }
-  })
+   type Query {
+     myCat: Cat
+   }
 
-  await connect(config.dbUrl)
-  const { url } = await server.listen({ port: config.port })
+   schema {
+     query: Query
+   }
+  `;
+	const schemaTypes = await Promise.all(types.map(loadTypeSchema));
 
-  console.log(`GQL server ready at ${url}`)
-}
+	const server = new ApolloServer({
+		typeDefs: [ rootSchema ],
+		resolvers: {
+			Query: {
+				myCat() {
+					return { name: 'Garfield' };
+				}
+			}
+		},
+		context({ req }) {
+			// use the authenticate function from utils to auth req, its Async!
+			return { user: null };
+		}
+	});
+
+	await connect(config.dbUrl);
+	const { url } = await server.listen({ port: config.port });
+
+	console.log(`GQL server ready at ${url}`);
+};
