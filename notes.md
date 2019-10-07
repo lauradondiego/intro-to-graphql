1deed4ec543a45d5c62478179c0a5fd7a3537497
laura <lauradondiego@gmail.com>
2019-10-06 23:13:45 -0400

    lessson 2

--- notes.md
+++ notes.md
@@ -14,3 +14,10 @@
 - npm run test-schema in terminal
 - all the tests will fail at first, look what theyre looking for and add the code
 - you will find all info needed for the code you are writing in the tests folder, in the product.type.spec file
+
+# Resolvers
+- resolvers: resolve queries, mutations, anything of value, enums, etc
+- they are like controllers that talk to the db but they resolve all the way down, so if a type referes to a field with another type that refers to another type
+- create a resolver in server.js based on the const rootschema you provide
+- now run the server and type this into the plaground:
+- { cat(name: 'Timmy') { name age owner{ name}, }}
--- src/server.js
+++ src/server.js
@@ -1,47 +1,59 @@
-import { ApolloServer } from 'apollo-server';
-import { loadTypeSchema } from './utils/schema';
-import { merge } from 'lodash';
-import config from './config';
-import { connect } from './db';
-import product from './types/product/product.resolvers';
-import coupon from './types/coupon/coupon.resolvers';
-import user from './types/user/user.resolvers';
+import { ApolloServer } from 'apollo-server'
+import { loadTypeSchema } from './utils/schema'
+import { merge } from 'lodash'
+import config from './config'
+import { connect } from './db'
+import product from './types/product/product.resolvers'
+import coupon from './types/coupon/coupon.resolvers'
+import user from './types/user/user.resolvers'
 
-const types = [ 'product', 'coupon', 'user' ];
+const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
-	const rootSchema = `
+  const rootSchema = `
    type Cat {
-     name: String
+	 owner: Owner!
+	 name: String!
+	 age: Int
+   }
+
+
+   type Owner {
+	name: String
+	cat: Cat!
    }
 
    type Query {
-     myCat: Cat
+	   cat(name: String!): Cat!
+	   owner(name: String!): Owner!
    }
 
    schema {
      query: Query
    }
-  `;
-	const schemaTypes = await Promise.all(types.map(loadTypeSchema));
+  `
+  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
-	const server = new ApolloServer({
-		typeDefs: [ rootSchema ],
-		resolvers: {
-			Query: {
-				myCat() {
-					return { name: 'Garfield' };
-				}
-			}
-		},
-		context({ req }) {
-			// use the authenticate function from utils to auth req, its Async!
-			return { user: null };
-		}
-	});
+  const server = new ApolloServer({
+    typeDefs: [rootSchema],
+    resolvers: {
+      Query: {
+        cat(_, args) {
+          return { name: args.name, age: 3, owner: {} }
+        },
+        owner(_, args) {
+          return { name: args.name, cat: {} }
+        }
+      }
+    },
+    context({ req }) {
+      // use the authenticate function from utils to auth req, its Async!
+      return { user: null }
+    }
+  })
 
-	await connect(config.dbUrl);
-	const { url } = await server.listen({ port: config.port });
+  await connect(config.dbUrl)
+  const { url } = await server.listen({ port: config.port })
 
-	console.log(`GQL server ready at ${url}`);
-};
+  console.log(`GQL server ready at ${url}`)
+}
